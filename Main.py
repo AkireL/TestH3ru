36d1512ae400f2d7b5409f5d312173fa286a037b
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2020-06-25 22:47:15 -0500

    add Readme

--- Main.py
+++ Main.py
@@ -9,9 +9,8 @@ import Constant
 class Main:
     def __init__(self):
         self.ch = CtrlHeuristic()
-    def do(self):
+    def do(self, textL):
         
-        textL = 'shoce pq podciy nfwh phfer epgdc dgsloqe do rhfl qhmoixw cmfur qdrulxogji whc ermjdhsx py en ycoienqm wjuln dwuch qinhmjul mjxdqfrnlg iygsex qihmu grewyluhfs ucf us xclpedqmi yrx qinexwo qx rqwwxflpdn rsogxd cpqmxj lgchqdin fdw nwcrus coj nj qplfjnwidg fwdmslqn cwj hysucxdqm ms hdmwpeigxweo sqflo ycqlinro ghu hgecdfj mw xrpmyenq fgixsr fpwcnguieh fclgj ghepqyd jxhwe cejfugn ujxqhihncrl mlceo udr fm ocxfsjdng sfoqmd pdoymnwxei spqinedf ql ncsepfl icmqsdj chwjlg yiq ifl syejrqd lwnepmcg xlmnfqry ghlyopuncw qx iw sionpux cop dmqpchuyf ojxfqhernm ignpeyf rseoyl emjocsildrfimdy mwd oewgjfr uo irmcunfgx ylduwpsnh xrdng gcxr ng prfmjicud srdueqhgiy nmodwsqijh dcnql'
         response = self.ch.execute(textL)
         print(f"Input Text: \n\t {textL}\n")
         print("1) There are(is) "+ str(response[Constant.HERUGLON_NAMES[Constant.HERUGLON_PREPOSITION]]) + " preposition(s) in the text")
@@ -22,5 +21,6 @@ class Main:
         print("5) There are(is) "+ str(response[Constant.HERUGLON_NAMES[Constant.HERUGLON_NUMBER_PRETTY]]) + " distinct pretty number(s) in the text")
         
         
+textL = 'shoce pq podciy nfwh phfer epgdc dgsloqe do rhfl qhmoixw cmfur qdrulxogji whc ermjdhsx py en ycoienqm wjuln dwuch qinhmjul mjxdqfrnlg iygsex qihmu grewyluhfs ucf us xclpedqmi yrx qinexwo qx rqwwxflpdn rsogxd cpqmxj lgchqdin fdw nwcrus coj nj qplfjnwidg fwdmslqn cwj hysucxdqm ms hdmwpeigxweo sqflo ycqlinro ghu hgecdfj mw xrpmyenq fgixsr fpwcnguieh fclgj ghepqyd jxhwe cejfugn ujxqhihncrl mlceo udr fm ocxfsjdng sfoqmd pdoymnwxei spqinedf ql ncsepfl icmqsdj chwjlg yiq ifl syejrqd lwnepmcg xlmnfqry ghlyopuncw qx iw sionpux cop dmqpchuyf ojxfqhernm ignpeyf rseoyl emjocsildrfimdy mwd oewgjfr uo irmcunfgx ylduwpsnh xrdng gcxr ng prfmjicud srdueqhgiy nmodwsqijh dcnql'
 m = Main()
-m.do()
+m.do(textL)
---
+++ Readme.md
@@ -0,0 +1,39 @@
+|||
+---|---|
+|Author| Erika Leonor Basurto Munguia|
+|Version| 1.0.0|
+|Date| 25/06/2020|
+
+# Test Heruglon Language
+Their alphabet, in order, is: `sxocqnmwpfyheljrdgui`.
+
+### Letter Classification
+letters u, d, x, s, m, p, f are called "foo letters" while the other letters are called "bar letters".
+
+### Prepositions
+The prepositions are the words of exactly 6 letters which end in a foo letter and do not contain the letter u .
+### Verbs
+Verbs are words of 6 letters or more that end in a bar letter. Furthermore, if a verb starts in a bar letter,
+then the verb is inflected in its subjunctive form.
+### Numbers
+Words also represent numbers given in base 20, where each letter is a
+digit. The digits are ordered from the least significant to the most significant, which is the
+opposite of our system. 
+
+Heruglons consider a number to be pretty if it satisfies all of the following properties:
+- it is greater than or equal to 81827
+- it is divisible by 3
+
+### Output
+```txt
+1) There are <n> prepositions in the text
+2) There are <n> verbs in the text
+3) There are <n> subjunctive verbs in the text
+4) Vocabulary list: <words>
+5) There are <n> distinct pretty numbers in the text
+```
+
+### How to run the script?
+```bash
+python Main.py
+```
